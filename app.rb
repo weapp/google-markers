b85d0eda4b558d134576d10114661ac5fc26bdb0
Manuel Albarrán <weap88@gmail.com>
2016-11-27 14:58:37 +0100

    Refactoed ruby

--- app.rb
+++ app.rb
@@ -12,34 +12,99 @@ String.class_eval do
   end
 end
 
-class GMaps < Sinatra::Base
+class Sockets
+  attr_reader :sockets
 
-  def broadcast_location(location)
-    msg = location.merge(type: 'location').to_json
-    GMaps.sockets.each { |socket| socket.send(msg) }
+  def initialize
+    @sockets = []
   end
 
+  def broadcast(*args)
+    sockets.each { |socket| socket.send(*args) }
+  end
+
+  def <<(socket)
+    sockets << socket
+  end
+
+  def delete(ws)
+    sockets.delete(ws)
+  end
+end
+
+class JsonWebSocket
+  attr_reader :socket
+
+  def initialize(socket)
+    @socket = socket
+  end
+
+  def onopen
+    @socket.onopen { yield }
+  end
+
+  def onmessage
+    @socket.onmessage do |message|
+      EM.next_tick { yield JSON.parse(message) }
+    end
+  end
+
+  def onclose
+    @socket.onclose { yield }
+  end
+
+  def send(type, message = nil)
+    if message
+      send(message.merge(type: type))
+    else
+      @socket.send(type.to_json)
+    end
+  end
+end
+
+class JsonRedis
+  attr_reader :redis
+
+  def initialize
+    EM.next_tick { @redis ||= EM::Hiredis.connect }
+  end
+
+  def subscribe(channel)
+    EM.next_tick do
+      redis.pubsub.subscribe(channel) do |msg|
+        EM.next_tick { yield JSON.parse(msg) }
+      end
+    end
+  end
+
+  def publish(channel, value)
+    redis.publish(channel, value.to_json)
+  end
+end
+
+class GMaps < Sinatra::Base
   def broadcast_location_by_ip(ip)
-    p "byip", ip
+    p byip: ip
     location = Geo.from(ip)
-    broadcast_location(location) if location
+    GMaps.sockets.broadcast('location', location) if location
+  end
+
+  def link_redis_to_socket(redis_channel, socket_type)
+    settings.redis.subscribe(redis_channel) do |value|
+      settings.sockets.broadcast(socket_type, value)
+    end
   end
 
   def initialize
     super
-
-    EM.next_tick do
-      @redis ||= EM::Hiredis.connect
-      @redis.pubsub.subscribe('locations') do |msg|
-        EM.next_tick { broadcast_location(JSON.parse(msg)) }
-      end
-    end
+    link_redis_to_socket('locations', 'location')
   end
   # threaded - False: Will take requests on the reactor thread
   #            True:  Will queue request for background thread
   configure do
     set :threaded, false
-    set :sockets, []
+    set :sockets, Sockets.new
+    set :redis, JsonRedis.new
   end
 
   get '/' do
@@ -47,23 +112,23 @@ class GMaps < Sinatra::Base
       erb :index
     else
       request.websocket do |ws|
-        ws.onopen do
-          ws.send({type: 'msg', msg: 'Hello World!'}.to_json)
-          settings.sockets << ws
-          broadcast_location_by_ip(env["REMOTE_ADDR"])
+        jws = ::JsonWebSocket.new(ws)
+
+        jws.onopen do
+          jws.send(type: 'msg', msg: 'Hello World!')
+          settings.sockets << jws
+          broadcast_location_by_ip(env['REMOTE_ADDR'])
         end
-        ws.onmessage do |msg|
-          EM.next_tick do
-            settings.sockets.each do |s|
-              s.send(msg)
-              broadcast_location_by_ip(JSON[msg]["msg"])
-              @redis.publish("locations", sample_location.to_json)
-            end
-          end
+
+        jws.onmessage do |msg|
+          settings.sockets.broadcast(msg)
+          # broadcast_location_by_ip(msg["msg"])
+          settings.redis.publish('locations', sample_location)
         end
-        ws.onclose do
+
+        jws.onclose do
           warn('websocket closed')
-          settings.sockets.delete(ws)
+          settings.sockets.delete(jws)
         end
       end
     end
